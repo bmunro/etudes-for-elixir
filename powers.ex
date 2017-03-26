65cbcbc0fc7d5e225fa00717f0d80b4b0df09bbb
Ben Munro <benjamin.munro@gmail.com>
2017-03-26 22:34:39 +1100

    Etude 4.4 - Refactor power function to use tail recursion with an accumulator

--- powers.ex
+++ powers.ex
@@ -1,12 +1,18 @@
 defmodule Powers do
-  import Kernel, except: [raise: 2]
+  import Kernel, except: [raise: 2, raise: 3]
 
   def raise(x,n) do
     cond do
       n == 0 -> 1
-      n == 1 -> x
-      n > 0  -> x * raise(x, n-1)
+      n > 0  -> raise(x, n, 1)
       n < 0  -> 1.0 / raise(x, -n)
+    end
+  end
+
+  defp raise(x,n,acc) do
+    cond do
+      n == 0 -> acc
+      true   -> raise(x,n-1,x*acc)
     end
   end
 
