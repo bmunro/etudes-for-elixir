a6a1e40ecf65d72fa561d6e9dd70be8f17095a06
Ben Munro <benjamin.munro@gmail.com>
2017-03-26 17:11:45 +1100

    Etude 3.4 - Tuple parameters & private functions

--- geom.ex
+++ geom.ex
@@ -8,14 +8,18 @@ defmodule Geom do
     Calculate the area of a geometric shape
   """
   @spec area(atom, number, number) :: number
-  def area(:rectangle, length, width) when length > 0 and width > 0 do
+  def area ({shape, dim1, dim2}) do
+    area(shape, dim1, dim2)
+  end
+
+  defp area(:rectangle, length, width) when length > 0 and width > 0 do
     length * width
   end
-  def area(:triangle, base, height) when base > 0 and height > 0 do
+  defp area(:triangle, base, height) when base > 0 and height > 0 do
     base * height * 0.5
   end
-  def area(:ellipse, radius1, radius2) when radius1 > 0 and radius2 > 0 do
+  defp area(:ellipse, radius1, radius2) when radius1 > 0 and radius2 > 0 do
     :math.pi * radius1 * radius2
   end
-  def area(_,_,_), do: 0
+  defp area(_,_,_), do: 0
 end
