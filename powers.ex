357f6a32f83364d9122558f1e1e8561c8c7fc736
Ben Munro <benjamin.munro@gmail.com>
2017-03-26 21:42:30 +1100

    Etude 4.3 - Power function - non tail recursive

---
+++ powers.ex
@@ -0,0 +1,13 @@
+defmodule Powers do
+  import Kernel, except: [raise: 2]
+
+  def raise(x,n) do
+    cond do
+      n == 0 -> 1
+      n == 1 -> x
+      n > 0  -> x * raise(x, n-1)
+      n < 0  -> 1.0 / raise(x, -n)
+    end
+  end
+
+end
