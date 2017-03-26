d92fe7da8004791081d509a6e0e812bfdedda2ae
Ben Munro <benjamin.munro@gmail.com>
2017-03-26 14:42:55 +1100

    Etude 2.3 - Document the Geom module

--- geom.ex
+++ geom.ex
@@ -1,3 +1,15 @@
 defmodule Geom do
+  @moduledoc """
+    Exercises from the Etudes for Elixir book:
+    http://chimera.labs.oreilly.com/books/1234000001642/index.html
+  """
+
+  @doc """
+    Calculate the area of a rectangle
+
+    ## Parameters
+      - length: Length of the rectangle, default 1
+      - width:  Width of the rectangle, default 1
+  """
   def area(length \\ 1, width \\ 1), do: length * width
 end
