fcc2a3bf284526fc99b460f63e5ef8f79e894845
Ben Munro <benjamin.munro@gmail.com>
2017-03-27 21:29:50 +1100

    Etude 4.5 - Recursion with a helper function. Calculating roots

--- powers.ex
+++ powers.ex
@@ -16,4 +16,19 @@ defmodule Powers do
     end
   end
 
+  def nth_root(x,n) do
+    nth_root(x, n, x / 2.0)
+  end
+  defp nth_root(x,n,approx) do
+    IO.puts("Current guess is #{approx}")
+    f       = raise(approx, n) - x
+    f_prime = n * raise(approx, n-1)
+    next    = approx - f / f_prime
+    change  = abs(next - approx)
+    cond do
+      change < 1.0e-8 -> next
+      true            -> nth_root(x, n, next)
+    end
+  end
+
 end
